9e6acccf69fe5bc4173e2ad9a70db0a57a398b0e
THC-QA <Tristan.Clapp@academytrainee.com>
2020-03-26 22:58:21 +0000

    services 1-3 working

--- service_1/application/__init__.py
+++ service_1/application/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -7,15 +8,27 @@ app = Flask(__name__)
 def home():
     if request.method == "POST":
         details = request.form
-        name = details["name"]
+        character = details["name"]
         combat = details["type"]
         perk = details["perk"]
-        return redirect(url_for("/browse")
-    return render_template('home.html')
+        # character.update(combat)
+        # character.update(perk)
+        character = json.dumps(character)
+        combat = json.dumps(combat)
+        perk = json.dumps(perk)
+        print(character)
+        print(combat)
+        print(perk)
+        requests.post("http://localhost:5001/", json=combat)
+        requests.post("http://localhost:5002/", json=perk)
+        requests.post("http://localhost:5003/", json=character)
+        return redirect(url_for("browse"))
+    return render_template('home.html', title="home")
 
 @app.route('/browse', methods=['GET'])
 def browse():
-        response = requests.get('http://service4:5000/title')
+        response = requests.get('http://localhost:5003/')
         print(response)
         sentence = response.text
-        return render_template('browse.html', sentence = sentence, title = 'Browse')
+        print(sentence)
+        return sentence # render_template('browse.html', sentence = sentence, title = 'Browse')
