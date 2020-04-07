25336a8b3cfc63332440d26367ce8e6906c8109d
THC-QA <Tristan.Clapp@academytrainee.com>
2020-04-07 11:36:41 +0000

    urltest

--- tests/url_testing.py
+++ tests/url_testing.py
@@ -2,9 +2,9 @@ import urllib3
 
 def test_home():
     http = urllib3.PoolManager()
-    r = http.request('GET', "http://localhost:80/")
+    r = http.request('GET', "http://35.230.152.96/")
 
 def test_negative():
     http = urllib3.PoolManager()
-    r = http.request('GET', "http://localhost:80/daemon")
+    r = http.request('GET', "http://35.230.152.96/daemon")
     assert r.status == 404
