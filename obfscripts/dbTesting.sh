10cbb1b20e138c4aa20fa6cbf6012dda5bf70185
THC-QA <Tristan.Clapp@academytrainee.com>
2020-04-07 12:19:31 +0000

    paths

--- obfscripts/dbTesting.sh
+++ obfscripts/dbTesting.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /var/bin/jenkins/workspace/sfia_project_2/sourcetest.env
+source ./sourcetest.env
 python3 -m coverage run -m pytest tests/db_testing.py
 python3 -m coverage report
